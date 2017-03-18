7efcf08cc321876864c62e7db6a86e9de41d0062
TSiustis <TSiustis@users.noreply.github.com>
2017-03-18 19:28:58 +0200

    Update TextEditor.java

--- src/editor/TextEditor.java
+++ src/editor/TextEditor.java
@@ -8,13 +8,7 @@ public class TextEditor extends JTextPane{
     public final static String NAME = "SimpleEditor";
     public final static double VERSION = 1.0;
 
-    /**
-     * @param args
-     * @throws UnsupportedLookAndFeelException 
-     * @throws IllegalAccessException 
-     * @throws InstantiationException 
-     * @throws ClassNotFoundException 
-     */
+   
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
         new GUI().setVisible(true);
     }
